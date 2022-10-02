ecd4c2b837b67d2cf00e36b454cd53c106d3bd83
GodOfPasta <114845008+GodOfPasta@users.noreply.github.com>
2022-10-02 12:26:46 +0800

    Update Main.hs

--- LW1-QuadraticEquation/Main.hs
+++ LW1-QuadraticEquation/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-data Answer = TwoRoots {x1, x2 :: Double} | OneRoot {x :: Double} | NonValue
+data Answer = TwoRoots {x1, x2 :: Double} | OneRoot {x :: Double} | NoRoots
 
 main :: IO ()
 main = do 
@@ -10,13 +10,13 @@ main = do
     print . printAnswer $ solve (read a :: Double) (read b :: Double) (read c :: Double)
 
 printAnswer :: Answer -> String
-printAnswer NonValue = "No Solutions"
+printAnswer NoRoots = "No Solutions"
 printAnswer (OneRoot x) = show x
 printAnswer (TwoRoots x1 x2) = show x1 ++ "; " ++ show x2
 
 solve :: Double -> Double -> Double -> Answer
 solve a b c
-    | a == 0 || disc < 0 = NonValue
+    | a == 0 || disc < 0 = NoRoots
     | disc > 0 = TwoRoots {x1 = x1, x2 = x2}
     | otherwise = OneRoot {x = x1}
     where
