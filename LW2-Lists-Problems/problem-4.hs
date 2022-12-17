79e73954a1e51fadb36152f077e1c540e71856e8
GodOfPasta <114845008+GodOfPasta@users.noreply.github.com>
2022-12-17 11:59:05 +0800

    Update problem-4.hs

--- LW2-Lists-Problems/problem-4.hs
+++ LW2-Lists-Problems/problem-4.hs
@@ -1,7 +1,18 @@
+-- (*) Find the number of elements of a list.
+-- myLength [123, 456, 789]
+-- 3
+-- myLength "Hello, world!"
+-- 13
 module Main (main) where
 
 main :: IO ()
-main = print $ myLength "aaaaa"
+main = do
+    print . myLength $ "abcd"
+--  > 4
+    print . myLength $ []
+--  > 0
+    print . myLength $ [1, 2, 3, 4, 5, 6]
+--  > 6 
 
 myLength :: [a] -> Int
 myLength [] = 0
