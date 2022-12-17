29d0c0248037d4704de5d5c509c62d3ae48e659b
GodOfPasta <114845008+GodOfPasta@users.noreply.github.com>
2022-12-17 11:56:43 +0800

    Update problem-3.hs

--- LW2-Lists-Problems/problem-3.hs
+++ LW2-Lists-Problems/problem-3.hs
@@ -1,10 +1,25 @@
+-- (*) Find the K'th element of a list. The first element in the list is number 1.
+-- * (insert-at 'alfa '(a b c d) 2)
+-- (A ALFA B C D)
+
 module Main (main) where
 
 main :: IO ()
-main = print $ elementAt [1,2,3] 2
+main = do
+    print (elementAt "abcd" 2)
+--  > "b"
+    print (elementAt "X" 1000)
+-- "*** Exception: Not Enough Elements!
+-- CallStack (from HasCallStack):
+--   error, called at problem-3.hs:12:18 in main:Main
+    print (elementAt "abcd" 0)
+-- "*** Exception: Invalid Index!
+-- CallStack (from HasCallStack):
+--   error, called at problem-3.hs:15:18 in main:Main
 
 elementAt :: [a] -> Int -> a
 elementAt a b
-    | length a - 1 < b = error "Not Enough Elements!"
-    | b == 0 = head a
+    | b <= 0 = error "Invalid Index!"
+    | length a < b = error "Not Enough Elements!"
+    | b == 1 = head a
     | otherwise = elementAt (tail a) (b-1)
