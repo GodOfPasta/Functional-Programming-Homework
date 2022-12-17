9609b168899cf45efa6c8c7bb5a8ad8932577470
GodOfPasta <114845008+GodOfPasta@users.noreply.github.com>
2022-12-17 11:21:13 +0800

    Update problem-2.hs

--- LW2-Lists-Problems/problem-2.hs
+++ LW2-Lists-Problems/problem-2.hs
@@ -4,6 +4,14 @@ main :: IO ()
 main = print $ getButLast "abcde"
 
 getButLast :: [a] -> a
-getButLast a 
-    |length a < 2 = error "Not Enough Elements"
-    |otherwise = last $ init a
+getButLast a
+    | length a <= 1 = error "Not Enough Elements"
+    | length a == 2 = head a
+    | otherwise = getButLast $ tail a
+
+--Вариант вообще без встроенных ф-й, но он мне не нравится
+
+-- getButLast [] = error "Not Enough Elements"
+-- getButLast [a] = error "Not Enough Elements"
+-- getButLast [a, b] = a
+-- getButLast (x:xs) = getButLast xs
