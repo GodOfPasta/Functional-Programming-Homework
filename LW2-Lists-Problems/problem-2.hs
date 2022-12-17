d5e223578d2297d6b6ba6ce486599a80ae8c22ee
GodOfPasta <114845008+GodOfPasta@users.noreply.github.com>
2022-12-17 11:49:56 +0800

    Update problem-2.hs

--- LW2-Lists-Problems/problem-2.hs
+++ LW2-Lists-Problems/problem-2.hs
@@ -1,17 +1,24 @@
+-- (*) Find the last but one element of a list.
+-- myButLast [1,2,3,4]
+-- 3
+-- myButLast ['a'..'z']
+-- 'y'
+
 module Main (main) where
 
 main :: IO ()
-main = print $ getButLast "abcde"
+main = do
+    print $ getButLast ""
+-- "*** Exception: Not Enough Elements
+-- CallStack (from HasCallStack):
+--   error, called at problem-2.hs:11:18 in main:Main
+    print $ getButLast "abcde"
+--  > "d"
+    print $ getButLast [0, 1, 2, 3]
+--  > "2"
 
 getButLast :: [a] -> a
 getButLast a
     | length a <= 1 = error "Not Enough Elements"
     | length a == 2 = head a
     | otherwise = getButLast $ tail a
-
---Вариант вообще без встроенных ф-й, но он мне не нравится
-
--- getButLast [] = error "Not Enough Elements"
--- getButLast [a] = error "Not Enough Elements"
--- getButLast [a, b] = a
--- getButLast (x:xs) = getButLast xs
