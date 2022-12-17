fb6acf89d797e5496a4a7d9e5bdbe26b763bab1a
GodOfPasta <114845008+GodOfPasta@users.noreply.github.com>
2022-12-17 11:42:34 +0800

    Update my-ho-functions.hs

--- LW4-High-Order-Functions/my-ho-functions.hs
+++ LW4-High-Order-Functions/my-ho-functions.hs
@@ -3,7 +3,7 @@ filter' :: (a -> Bool) -> [a] -> [a]
 filter' _ [] = []
 filter' f (x: xs)
     | f x = x : filter' f xs
-    | otherwise = filter f xs
+    | otherwise = filter' f xs
 
 -- Ф-я flip
 flip' :: (a -> b -> c) -> b -> a -> c
