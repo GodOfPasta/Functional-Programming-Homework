7383985ec88dae3c34fa2bb73144fd3a74431341
GodOfPasta <114845008+GodOfPasta@users.noreply.github.com>
2022-12-17 11:28:14 +0800

    Update problem-3.hs

--- LW2-Lists-Problems/problem-3.hs
+++ LW2-Lists-Problems/problem-3.hs
@@ -4,6 +4,7 @@ main :: IO ()
 main = print $ elementAt [1,2,3] 2
 
 elementAt :: [a] -> Int -> a
-elementAt a b 
-    |   b < 0 || b >= length a = error "Current Index not Allowed"
-    |   otherwise = a !! b
+elementAt a b
+    | length a - 1 < b = error "Not Enough Elements!"
+    | b == 0 = head a
+    | otherwise = elementAt (tail a) (b-1)
