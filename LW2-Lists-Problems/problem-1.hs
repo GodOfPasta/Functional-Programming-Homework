431bd9621113ce988795c33ce9304b440b6ff72e
GodOfPasta <114845008+GodOfPasta@users.noreply.github.com>
2022-12-17 11:47:54 +0800

    Update problem-1.hs

--- LW2-Lists-Problems/problem-1.hs
+++ LW2-Lists-Problems/problem-1.hs
@@ -1,7 +1,19 @@
+-- (*) Find the last element of a list.
+-- myLast [1,2,3,4]
+-- 4
+-- myLast ['x','y','z']
+-- 'z'
+
 module Main (main) where
 
 main :: IO ()
-main = print $ getLast ""
+main = do
+    print $ getLast ""
+--  > ""
+    print $ getLast "abcde"
+--  > "e"
+    print $ getLast [0, 1, 2, 3]
+--  > "3"
 
 getLast ::[a] -> a
 getLast [] = error "Not Enough Elements!"
