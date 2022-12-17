35a94619698084d27778549f34d84a58923926f6
GodOfPasta <114845008+GodOfPasta@users.noreply.github.com>
2022-12-17 12:02:55 +0800

    Update problem-5.hs

--- LW2-Lists-Problems/problem-5.hs
+++ LW2-Lists-Problems/problem-5.hs
@@ -1,7 +1,20 @@
+-- (*) Reverse a list.
+-- myReverse "A man, a plan, a canal, panama!"
+-- "!amanap ,lanac a ,nalp a ,nam A"
+-- myReverse [1,2,3,4]
+-- [4,3,2,1]
+
 module Main (main) where
 
 main :: IO ()
-main = print $ reverseList ""
+main = do
+    print . reverseList $ [1, 2, 3, 4, 5]
+--  > [5, 4, 3, 2, 1]
+    print . reverseList $ ""
+--  > ""
+    print . reverseList $ "abcd"
+-- "dcba"
+
 reverseList :: [a] -> [a]
 reverseList  [] = []
 reverseList  (x:xs) = reverseList xs ++ [x]
